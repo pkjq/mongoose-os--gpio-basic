41c81f477456ecb8b6b7064c21b7c51160a8c53b
SD <namelosted@gmail.com>
2020-02-09 17:21:08 +0300

    add gpios descriptions

--- gpio_esp.h
+++ gpio_esp.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 pkjq
+ * Copyright (c) 2018-2020 pkjq
  * All rights reserved
  *
  * Licensed under the Apache License, Version 2.0 (the ""License"");
@@ -21,6 +21,34 @@
 
 namespace gpio
 {
+// Pins used during Boot:
+// The ESP8266 can be prevented from booting if some pins are pulled LOW or HIGH.
+// The following list shows the state of the following pins on BOOT:
+//      * GPIO16: pin is high at BOOT
+//      * GPIO0:  boot failure if pulled LOW
+//      * GPIO2:  pin is high on BOOT, boot failure if pulled LOW
+//      * GPIO15: boot failure if pulled HIGH
+//      * GPIO3:  pin is high at BOOT
+//      * GPIO1:  pin is high at BOOT, boot failure if pulled LOW
+//      * GPIO10: pin is high at BOOT
+//      * GPIO9:  pin is high at BOOT
+
+
+// Pins HIGH at Boot:
+// There are certain pins that output a 3.3V signal when the ESP8266 boots.
+// The following GPIOs output a HIGH signal on boot:
+//      * GPIO16
+//      * GPIO3
+//      * GPIO1
+//      * GPIO10
+//      * GPIO9
+
+
+// GPIOs connected to the Flash Chip:
+//      * GPIO6 to GPIO11 are usually connected to the flash chip in ESP8266 boards.
+// This pins are not recommended to use.
+
+
 enum class ESP01: int
 {
     Tx          = 1,
